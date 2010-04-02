e3101bc2970eea196ca37c2c4238c4f30493a38b
Joel Hansson <joel.hansson@gmail.com>
2010-04-02 15:11:13 +0200

    first iteration

--- flip-disc-widge.rb
+++ flip-disc-widge.rb
@@ -1,6 +1,30 @@
 require 'rubygems'
 require 'sinatra'
+require 'lib/simple_helper'
+
+helpers do
+  include SimpleHelper
+end
 
 get "/" do
-  "This is the flip-disc display widget/helper!"
+  haml :index
 end
+
+post "/" do
+  @display_str = sanitize params[:str]
+  haml :success
+end
+
+__END__
+
+@@layout
+!!! 5
+%html
+  %head
+    %title flip-disc display widgy!
+    %link{:rel=>"stylesheet", :href=>"/stylesheets/reset.css"}
+    %link{:rel=>"stylesheet", :href=>"/stylesheets/style.css"}
+  %body
+    = yield
+
+
